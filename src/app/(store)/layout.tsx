64ed91fb2817b12f245f92c42c6d3839737e2e29
Luís <luisf.csdev@gmail.com>
2023-12-04 21:06:37 -0300

    feat: built home

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="gap 5 mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app p-8">
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
       <Header />
       {children}
     </div>
